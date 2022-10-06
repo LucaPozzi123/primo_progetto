d3f1b2b44307b5113f0a166ee68a763157aba646
LucaPozzi123 <98452224+LucaPozzi123@users.noreply.github.com>
2022-10-06 12:40:12 +0200

    Update settings.py

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--8c26_k*2g)bl3s2z169m5k7h$rln)!z55=!09k79a0vdcb4ss
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
