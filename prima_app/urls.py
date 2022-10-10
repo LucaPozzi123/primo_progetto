f2c79bb969488211ee996606c3dfe159f65a5a25
LucaPozzi123 <98452224+LucaPozzi123@users.noreply.github.com>
2022-10-10 09:15:52 +0200

    main

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import homepage,welcome,lista,variabili,index,chi_siamo
 app_name="prima_app"
 urlpatterns=[
-    path('',homepage,name='homepage'),
+    path('homepage',homepage,name='homepage'),
     path('welcome',welcome,name='welcome'),
     path('lista',lista,name='lista'),
      path('chi_siamo',chi_siamo,name='chi_siamo'),
     path('variabili',variabili,name='variabili'),
-    path('index',index,name='index')
+    path('',index,name='index')
    
 
 
