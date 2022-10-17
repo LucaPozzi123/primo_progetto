dc178b3f3fb0a2cc164bb72da64c7488ccf6de63
LucaPozzi123 <98452224+LucaPozzi123@users.noreply.github.com>
2022-10-17 08:39:00 +0200

    passo 9

---
+++ seconda_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import es_if,if_else_elif
+app_name="seconda_app"
+urlpatterns=[
+    path('',es_if,name='es_if'),
+    path('if_else_elif',if_else_elif,name='if_else_elif'),
+
+]
