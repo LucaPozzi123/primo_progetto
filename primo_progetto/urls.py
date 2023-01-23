a9ecbc305ff29dc53cda2f675d0451471ceec3f3
LucaPozzi123 <98452224+LucaPozzi123@users.noreply.github.com>
2023-01-23 09:47:14 +0100

    nano

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     path("prova_pratica_0", include("prova_pratica_0.urls", namespace="prova_pratica_0")),
     path("prova_pratica_2", include("prova_pratica_2.urls", namespace="prova_pratica_2")),
 ]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
 
 
